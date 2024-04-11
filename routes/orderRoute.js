de8c7b1cb3f691f0430eeb3f71745155bc158f24
Sasidev <conorken1@gmail.com>
2024-04-11 23:42:02 +0530

    Update orderRoute.js
    Added route to CreateCustomOrder

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -4,5 +4,6 @@ const orderController = require('../controllers/orderController.js');
 const router = Router();
 
 router.get('/orders', orderController.getAllOrders);
+router.post(`/orders`,orderController.createCustomOrder);
 
 module.exports = router;
