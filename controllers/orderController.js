5305902de1f6a89444c354015a2a35792ff5b92c
0ju1c3 <otapadia@gmail.com>
2024-06-06 18:41:38 +0530

    Feat: createCustomOrder api call if payment success, changes to createCustomOrder

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -49,31 +49,61 @@ const createReturnOrder = async (req, res) => {
 
 //Use this API to create a quick custom order.
 //https://apidocs.shiprocket.in/#247e58f3-37f3-4dfb-a4bb-b8f6ab6d41ec
-const createCustomOrder = async (req, res) => {
-  const shiprocketToken = req.headers["Authorization"];
-  //details about order will be sent from frontend as orderData
-  let orderData = req.body;
+// const createCustomOrder = async (req, res) => {
+//   const shiprocketToken = req.headers["Authorization"];
+//   //details about order will be sent from frontend as orderData
+//   let orderData = req.body;
+//   const headers = {
+//     Authorization: shiprocketToken,
+//   };
+//   try {
+//     axios
+//       .post(
+//         `https://apiv2.shiprocket.in/v1/external/orders/create/adhoc`,
+//         orderData,
+//         { headers },
+//       )
+//       .then((data) => {
+//         return res.json(data.data);
+//         //Save 'data.data.order ID' as we will use this in future API calls.
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//         return res.json(error);
+//         //error has message and status code
+//       });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+const createShiprocketOrder = async (shiprocketToken, orderData) => {
   const headers = {
     Authorization: shiprocketToken,
   };
   try {
-    axios
-      .post(
-        `https://apiv2.shiprocket.in/v1/external/orders/create/adhoc`,
-        orderData,
-        { headers },
-      )
-      .then((data) => {
-        return res.json(data.data);
-        //Save 'data.data.order ID' as we will use this in future API calls.
-      })
-      .catch((error) => {
-        console.log(error);
-        return res.json(error);
-        //error has message and status code
-      });
+    const response = await axios.post(
+      "https://apiv2.shiprocket.in/v1/external/orders/create/adhoc",
+      orderData,
+      { headers },
+    );
+    return response.data;
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    throw new Error("Failed to create order");
+  }
+};
+
+const createCustomOrder = async (req, res) => {
+  const shiprocketToken = req.headers["Authorization"];
+  const orderData = req.body;
+
+  try {
+    const data = await createShiprocketOrder(shiprocketToken, orderData);
+    return res.json(data);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: error.message });
   }
 };
 
@@ -134,4 +164,5 @@ module.exports = orderController = {
   createReturnOrder,
   cancelOrder,
   updateOrder,
+  createShiprocketOrder,
 };
--- controllers/razorpayController.js
+++ controllers/razorpayController.js
@@ -2,7 +2,7 @@ const crypto = require("crypto");
 const Razorpay = require("razorpay");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const orderController = require("./orderController");
+const { createShiprocketOrder } = require("./orderController");
 
 const getOrderId = async (req, res) => {
   try {
@@ -41,6 +41,7 @@ const paymentCallback = async (req, res) => {
   const { razorpay_signature, razorpay_payment_id, razorpay_order_id } =
     req.body;
   console.log(req.body);
+
   try {
     const string = `${razorpay_order_id}|${razorpay_payment_id}`;
 
@@ -50,7 +51,8 @@ const paymentCallback = async (req, res) => {
       .digest("hex");
 
     if (generated_signature == razorpay_signature) {
-      console.log("payment successfull");
+      console.log("payment successful");
+
       await prisma.order.update({
         where: {
           id: razorpay_order_id,
@@ -59,11 +61,29 @@ const paymentCallback = async (req, res) => {
           isPaid: true,
         },
       });
-      orderController.createCustomOrder(req, res);
+
+      const shiprocketToken = req.headers["Authorization"];
+      const orderData = {
+        //populate order data
+      }; //need to figure out how to get the order data
+
+      try {
+        const orderResponse = await createShiprocketOrder(
+          shiprocketToken,
+          orderData,
+        );
+        console.log("Order created:", orderResponse);
+      } catch (error) {
+        console.error("Failed to create order:", error.message);
+      }
+
       return res.redirect(successUrl);
+    } else {
+      return res.status(400).json({ error: "Invalid payment signature" });
     }
   } catch (error) {
     console.log(error.message);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
