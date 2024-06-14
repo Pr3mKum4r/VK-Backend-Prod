d03406908f929c43db9ae1dfa5b0723947c4b095
0ju1c3 <otapadia@gmail.com>
2024-06-15 02:02:20 +0530

    merge with main

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -3,20 +3,16 @@ const orderController = require("../controllers/orderController.js");
 
 const router = Router();
 
-<<<<<<< HEAD
 router.get("/orders", orderController.getAllOrders);
 router.post("/orders/update", orderController.updateOrder);
 router.post("/orders/cancel", orderController.cancelOrder);
-=======
-router.get('/orders', orderController.getAllOrders);
-
-
-router.post(`/orders`,orderController.createCustomOrder);
-router.get('/trackorder/:orderID/:channelID',orderController.getTrackingDataWithOrderId);
-router.get('/trackorder/:orderID',orderController.getTrackingDataWithOrderId);
->>>>>>> origin/main
-
 router.post(`/orders`, orderController.createCustomOrder);
+router.get(
+  "/trackorder/:orderID/:channelID",
+  orderController.getTrackingDataWithOrderId,
+);
+router.get("/trackorder/:orderID", orderController.getTrackingDataWithOrderId);
 
 router.post("/orders/create/return", orderController.createReturnOrder); //Create a return order
+
 module.exports = router;
